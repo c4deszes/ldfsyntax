9f0ab6768d8ef0de809da7886c50070506f4687c
Balazs Eszes <c4deszes@gmail.com>
2021-11-22 19:09:46 +0100

    Definition testing

--- server/server.py
+++ server/server.py
@@ -1,6 +1,7 @@
 from pygls.capabilities import COMPLETION, TEXT_DOCUMENT_DID_OPEN, TEXT_DOCUMENT_DID_CLOSE
-from pygls.lsp.methods import TEXT_DOCUMENT_DID_CHANGE
-from pygls.lsp.types.basic_structures import TextDocumentRegistrationOptions
+from pygls.lsp.methods import DEFINITION, TEXT_DOCUMENT_DID_CHANGE
+from pygls.lsp.types.basic_structures import Location, Position, Range, TextDocumentRegistrationOptions
+from pygls.lsp.types.language_features.definition import DefinitionParams
 from pygls.lsp.types.workspace import DidChangeTextDocumentParams, DidCloseTextDocumentParams, DidOpenTextDocumentParams
 from pygls.server import LanguageServer
 from pygls.lsp import CompletionItem, CompletionList, CompletionOptions, CompletionParams
@@ -30,5 +31,11 @@ def closeHandler(server: LanguageServer, params: DidCloseTextDocumentParams):
 @server.feature(TEXT_DOCUMENT_DID_CHANGE)
 def changeHandler(server: LanguageServer, params: DidChangeTextDocumentParams):
     server.show_message_log(f"Changed document: {params.text_document.uri}")
+
+@server.feature(DEFINITION)
+def onDefinition(server: LanguageServer, params: DefinitionParams):
+    word = server.workspace.get_document(params.text_document.uri).word_at_position(params.position)
+    server.show_message_log(f"Requesting definition at {params.position.line}:{params.position.character} Word: {word}")
+    return Location(uri=params.text_document.uri, range=Range(start=Position(line=0, character=0), end=Position(line=0, character=0)))
 
 server.start_io()
